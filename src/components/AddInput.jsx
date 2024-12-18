5bbd8ede6928e569ef7b8637f3058710386ec3e6
bashair0 <bisho5.ba@gmail.com>
2024-12-18 13:39:11 +0800

    Add add input component

---
+++ src/components/AddInput.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import "../styles/AddInput.css";
+
+export default function AddInput({ addTodo }) {
+  const [inputData, setInputData] = useState({ todo: "" });
+
+  const handleChange = (evt) => {
+    setInputData((currentData) => {
+      return {
+        ...currentData,
+        [evt.target.name]: [evt.target.value],
+      };
+    });
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    addTodo(inputData);
+    setInputData({ todo: "" });
+  };
+  return (
+    <form action="post" onSubmit={handleSubmit} className="Form">
+      <input
+        className="AddInput"
+        type="text"
+        placeholder="Create a new todo..."
+        id="todo"
+        name="todo"
+        value={inputData.todo}
+        onChange={handleChange}
+      />
+    </form>
+  );
+}
